a6896fd02e8fdb09c77d5659c49111dd90e3d70b
p1re <oriolnieto2006@gmail.com>
2025-02-08 11:12:15 +0100

    feat buscar.java
    Added two functions to search using name or origin.

--- src/proyecto/buscar.java
+++ src/proyecto/buscar.java
@@ -1,5 +1,31 @@
 package proyecto;
 
-public class buscar {
+import java.util.Scanner;
 
+public class buscar {
+	public static void porNombre(String[] hierba) {
+		   Scanner scan = new Scanner(System.in);
+
+		   System.out.println("Qué hierba quieres buscar por nombre?");
+		   String hierbaABuscarNombre = scan.nextLine();
+
+		   for (int i = 0; i < hierba.length; i++) {
+		       if (hierba[i].equalsIgnoreCase(hierbaABuscarNombre)) {
+		           System.out.println("Hierba encontrada!");
+		       }
+		   }
+		}
+
+	public static void porOrigen(String[] origen, String[] hierba) {
+		   Scanner scan = new Scanner(System.in);
+
+		   System.out.println("Qué hierba quieres buscar por origen?");
+		   String hierbaABuscarOrigen = scan.nextLine();
+
+		   for (int i = 0; i < origen.length; i++) {
+		       if (origen[i].equalsIgnoreCase(hierbaABuscarOrigen)) {
+		           System.out.println("Hierba encontrada: " + hierba[i]);
+		       }
+		   }
+		}
 }
--- src/proyecto/main.java
+++ src/proyecto/main.java
@@ -1,5 +1,39 @@
 package proyecto;
 
 public class main {
+	public static String[] hierba = { "Maria Juana", "Hierba de los bosques", "La seta feliz", "El oro verde" };
+	public static String[] origen = { "Mexico", "Andorra", "Marrueco", "Palestina" };
+	public static double[] precio = { 1.70, 4.20, 2.47, 1.33 };
 
-}
+	public static void main(String[] args) {
+	String nombre, lugar;
+	double cantidad;
+	
+	System.out.println("Buenos dias! Que quieres hacer hoy?");
+	System.out.println("Usuario selecciona buscar por nombre");
+	buscar.porNombre(hierba);
+
+	System.out.println("Usuario selecciona buscar por origen");
+	buscar.porOrigen(origen,hierba);
+
+	System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
+	// Codigo para ordenar de mayor a menor precio
+	System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
+	// Codigo para ordenar de mayor a menor precio
+	System.out.println("Usuario selecciona varios productos para comprar");
+	// Codigo para seleccionar primer producto
+	nombre = "Hierba de los bosques";
+	cantidad = 3;
+	// Codigo para seleccionar segundo producto
+	nombre = "El oro verde";
+	cantidad = 1;
+	// Codigo para seleccionar tercer producto
+	nombre = "La seta feliz";
+	cantidad = 2.5;
+	System.out.println("Usuario elimina el segundo elemento de su carrito");
+	// Codigo para eliminar elemento del carrito
+	System.out.println("Usuario finaliza la compra");
+	// Codigo para finalizar la compra y que salga el total de la compra
+	}
+	//Funciones extra que considereis necesarias
+	}
