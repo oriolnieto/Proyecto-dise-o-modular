2b58466e54b99c06759793394d5fe53b00ec1d6e
p1re <oriolnieto2006@gmail.com>
2025-02-13 09:51:55 +0100

    feat ordenar.java
    Added two functions to order by min-max & max-min.

--- src/proyecto/ordenar.java
+++ src/proyecto/ordenar.java
@@ -1,5 +1,48 @@
 package proyecto;
 
 public class ordenar {
+	public static void ordenarMenorMayor(double[] precio, String[] hierba) {
+	       for (int i = 0; i < precio.length - 1; i++) {
+	           for (int j = i + 1; j < precio.length; j++) {
+	               if (precio[i] > precio[j]) {
+	                   double tempPrecio = precio[i];
+	                   precio[i] = precio[j];
+	                   precio[j] = tempPrecio;
+	                   
 
+	                   String tempHierba = hierba[i];
+	                   hierba[i] = hierba[j];
+	                   hierba[j] = tempHierba;
+	               }
+	           }
+	       }
+	       
+	       System.out.println("Precio ordenado de menor a mayor:");
+	       for (int i = 0; i < precio.length; i++) {
+	           System.out.println(hierba[i] + ": " + precio[i] + "€");
+	       }
+	       System.out.println();
+	   }
+
+	public static void ordenarMayorMenor(double[] precio, String[] hierba) {
+	       for (int i = 0; i < precio.length - 1; i++) {
+	           for (int j = i + 1; j < precio.length; j++) {
+	               if (precio[i] < precio[j]) {
+	                   double tempPrecio = precio[i];
+	                   precio[i] = precio[j];
+	                   precio[j] = tempPrecio;
+	                   
+	                   String tempHierba = hierba[i];
+	                   hierba[i] = hierba[j];
+	                   hierba[j] = tempHierba;
+	               }
+	           }
+	       }
+	       
+	       System.out.println("Precio ordenado de mayor a menor:");
+	       for (int i = 0; i < precio.length; i++) {
+	           System.out.println(hierba[i] + ": " + precio[i] + "€");
+	       }
+	       System.out.println();
+	   }
 }
