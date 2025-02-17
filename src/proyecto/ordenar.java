395f5b3660bb5a6b532bd93f756c234c6f63757c
p1re <oriolnieto2006@gmail.com>
2025-02-17 10:15:20 +0100

    style ordenar.java
    Added comments to ordenar.java functions.

--- src/proyecto/ordenar.java
+++ src/proyecto/ordenar.java
@@ -4,22 +4,22 @@ public class ordenar {
 	public static void menorMayor(double[] precio, String[] hierba) {
 	       for (int i = 0; i < precio.length - 1; i++) {
 	           for (int j = i + 1; j < precio.length; j++) {
-	               if (precio[i] > precio[j]) {
-	                   double tempPrecio = precio[i];
+	               if (precio[i] > precio[j]) { // Si el precio[i] es mas grande que el que tenemos almacenado en j.
+	                   double temporalPrecio = precio[i];
 	                   precio[i] = precio[j];
-	                   precio[j] = tempPrecio;
+	                   precio[j] = temporalPrecio; // Cambiamos precio[i] a precio[j].
 	                   
 
-	                   String tempHierba = hierba[i];
+	                   String temporalHierba = hierba[i];
 	                   hierba[i] = hierba[j];
-	                   hierba[j] = tempHierba;
+	                   hierba[j] = temporalHierba;
 	               }
 	           }
 	       }
 	       
 	       System.out.println("Precio ordenado de menor a mayor:");
 	       for (int i = 0; i < precio.length; i++) {
-	           System.out.println(hierba[i] + ": " + precio[i] + "€");
+	           System.out.println(hierba[i] + ": " + precio[i] + "€"); // Mostramos los precios ordenados de menor a mayor.
 	       }
 	       System.out.println();
 	   }
@@ -28,13 +28,13 @@ public class ordenar {
 	       for (int i = 0; i < precio.length - 1; i++) {
 	           for (int j = i + 1; j < precio.length; j++) {
 	               if (precio[i] < precio[j]) {
-	                   double tempPrecio = precio[i];
+	                   double temporalPrecio = precio[i];
 	                   precio[i] = precio[j];
-	                   precio[j] = tempPrecio;
+	                   precio[j] = temporalPrecio;
 	                   
-	                   String tempHierba = hierba[i];
+	                   String temporalHierba = hierba[i];
 	                   hierba[i] = hierba[j];
-	                   hierba[j] = tempHierba;
+	                   hierba[j] = temporalHierba;
 	               }
 	           }
 	       }
