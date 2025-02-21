be08fcb8416992df4c6c1c5072b12a9ea53af32b
p1re <oriolnieto2006@gmail.com>
2025-02-21 10:03:51 +0100

    feat buscar.java
    Improved the search conditions by adding contains.

--- src/proyecto/buscar.java
+++ src/proyecto/buscar.java
@@ -7,12 +7,17 @@ public class buscar {
 		   Scanner scan = new Scanner(System.in);
 		   boolean hierbaEncontrada = false;
 
-		   System.out.println("Qué hierba quieres buscar por nombre?"); // Pedimos al usuario la hierba y la guardamos para luego comparar.
-		   String hierbaABuscarNombre = scan.nextLine();
+		   System.out.println("Que hierba quieres buscar por nombre?"); // Pedimos al usuario la hierba y la guardamos para luego comparar.
+		   String hierbaABuscar = scan.nextLine();
 
 		   for (int i = 0; i < hierba.length; i++) {
-		       if (hierba[i].equalsIgnoreCase(hierbaABuscarNombre)) {   // Buscar si la hierba hace match con nuestras hierbas, ignorando mayusculas e minusculas.
-		           System.out.println("Hierba encontrada: " + hierba[i] + " | " + "Origen: " + origen[i] + " | " + "Precio: " + precio[i] + "€");
+			   if (hierba[i].toLowerCase().contains(hierbaABuscar)) {
+		           System.out.println("Posible Coincidencia: " + hierba[i] + " | " + "Origen: " + origen[i] + " | " + "Precio: " + precio[i] + "€"); // Buscar si la hierba tiene coincidencias
+		           System.out.println();
+		           hierbaEncontrada = true;
+		       }
+		       else if (hierba[i].equalsIgnoreCase(hierbaABuscar)) {
+		           System.out.println("Hierba encontrada: " + hierba[i] + " | " + "Origen: " + origen[i] + " | " + "Precio: " + precio[i] + "€");  // Buscar si la hierba hace match con nuestras hierbas, ignorando mayusculas e minusculas.
 		           System.out.println();
 		           hierbaEncontrada = true;
 		       }
@@ -28,11 +33,16 @@ public class buscar {
 		   Scanner scan = new Scanner(System.in);
 		   boolean hierbaEncontrada = false;
 
-		   System.out.println("Qué hierba quieres buscar por origen?");
-		   String hierbaABuscarOrigen = scan.nextLine();
+		   System.out.println("Que hierba quieres buscar por origen?");
+		   String origenABuscar = scan.nextLine();
 
 		   for (int i = 0; i < origen.length; i++) {
-		       if (origen[i].equalsIgnoreCase(hierbaABuscarOrigen)) {
+			   if (origen[i].toLowerCase().contains(origenABuscar)) {
+		           System.out.println("Posible Coincidencia: " + hierba[i] + " | " + "Origen: " + origen[i] + " | " + "Precio: " + precio[i] + "€");
+		           System.out.println();
+		           hierbaEncontrada = true;
+		       }
+		       else if (origen[i].equalsIgnoreCase(origenABuscar)) {
 		           System.out.println("Hierba encontrada: " + hierba[i] + " | " + "Origen: " + origen[i] + " | " + "Precio: " + precio[i] + "€");
 		           System.out.println();
 		           hierbaEncontrada = true;
